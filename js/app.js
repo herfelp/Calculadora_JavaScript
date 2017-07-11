933a1e439dc62c06f03acf529907a6856ec05338
Hernán Felipe Castro <hernan.castro.j@gmail.com>
2017-07-11 08:42:41 -0400

    mejora en método de encontrar el largo de caracteres de la pantalla

--- js/app.js
+++ js/app.js
@@ -17,16 +17,24 @@
 
 
 
-
-
+function pant(){
 var display = document.getElementById("display").innerHTML;
+return(display);
+};
+
+function lpan(){
+  var disp = document.getElementById("display").innerHTML.replace(/ /g,"");
+  var disp_a = disp.replace("-","");
+  var disp_b = disp_a.replace(".","");
+  var l = disp_b.length;
+  return(l);
+};
+
+
 
 var tecla9 = document.getElementById("9");
 tecla9.addEventListener("click",function(){
-       var disp = document.getElementById("display").innerHTML.replace(/ /g,"");
-       var disp_a = disp.replace("-","");
-       var disp_b = disp_a.replace(".","");
-if (disp_b.length < 8){
+if (lpan() < 8){
   if (document.getElementById("display").innerHTML=="0"){
      document.getElementById("display").innerHTML = document.getElementById("display").innerHTML.replace("0","9");
   }
@@ -34,14 +42,14 @@ if (disp_b.length < 8){
   document.getElementById("display").innerHTML = document.getElementById("display").innerHTML.concat("9");
   }
 }
+var dis = document.getElementById("display").innerHTML;
+
 });
 
 var tecla8 = document.getElementById("8");
 tecla8.addEventListener("click",function(){
-       var disp = document.getElementById("display").innerHTML.replace(/ /g,"");
-       var disp_a = disp.replace("-","");
-       var disp_b = disp_a.replace(".","");
-if (disp_b.length < 8){
+    
+if (lpan() < 8){
   if (document.getElementById("display").innerHTML=="0"){
      document.getElementById("display").innerHTML = document.getElementById("display").innerHTML.replace("0","8");
   }
@@ -53,10 +61,8 @@ if (disp_b.length < 8){
 
 var tecla7 = document.getElementById("7");
 tecla7.addEventListener("click",function(){
-       var disp = document.getElementById("display").innerHTML.replace(/ /g,"");
-       var disp_a = disp.replace("-","");
-       var disp_b = disp_a.replace(".","");
-if (disp_b.length < 8){
+
+if (lpan() < 8){
   if (document.getElementById("display").innerHTML=="0"){
      document.getElementById("display").innerHTML = document.getElementById("display").innerHTML.replace("0","7");
   }
@@ -68,10 +74,8 @@ if (disp_b.length < 8){
 
 var tecla6 = document.getElementById("6");
 tecla6.addEventListener("click",function(){
-       var disp = document.getElementById("display").innerHTML.replace(/ /g,"");
-       var disp_a = disp.replace("-","");
-       var disp_b = disp_a.replace(".","");
-if (disp_b.length < 8){
+
+if (lpan() < 8){
   if (document.getElementById("display").innerHTML=="0"){
      document.getElementById("display").innerHTML = document.getElementById("display").innerHTML.replace("0","6");
   }
@@ -83,10 +87,8 @@ if (disp_b.length < 8){
 
 var tecla5 = document.getElementById("5");
 tecla5.addEventListener("click",function(){
-       var disp = document.getElementById("display").innerHTML.replace(/ /g,"");
-       var disp_a = disp.replace("-","");
-       var disp_b = disp_a.replace(".","");
-if (disp_b.length < 8){
+
+if (lpan() < 8){
   if (document.getElementById("display").innerHTML=="0"){
      document.getElementById("display").innerHTML = document.getElementById("display").innerHTML.replace("0","5");
   }
@@ -98,10 +100,8 @@ if (disp_b.length < 8){
 
 var tecla4 = document.getElementById("4");
 tecla4.addEventListener("click",function(){
-       var disp = document.getElementById("display").innerHTML.replace(/ /g,"");
-       var disp_a = disp.replace("-","");
-       var disp_b = disp_a.replace(".","");
-if (disp_b.length < 8){
+
+if (lpan() < 8){
   if (document.getElementById("display").innerHTML=="0"){
      document.getElementById("display").innerHTML = document.getElementById("display").innerHTML.replace("0","4");
   }
@@ -113,10 +113,8 @@ if (disp_b.length < 8){
 
 var tecla3 = document.getElementById("3");
 tecla3.addEventListener("click",function(){
-       var disp = document.getElementById("display").innerHTML.replace(/ /g,"");
-       var disp_a = disp.replace("-","");
-       var disp_b = disp_a.replace(".","");
-if (disp_b.length < 8){
+
+if (lpan() < 8){
   if (document.getElementById("display").innerHTML=="0"){
      document.getElementById("display").innerHTML = document.getElementById("display").innerHTML.replace("0","3");
   }
@@ -128,10 +126,8 @@ if (disp_b.length < 8){
 
 var tecla2 = document.getElementById("2");
 tecla2.addEventListener("click",function(){
-       var disp = document.getElementById("display").innerHTML.replace(/ /g,"");
-       var disp_a = disp.replace("-","");
-       var disp_b = disp_a.replace(".","");
-if (disp_b.length < 8){
+
+if (lpan() < 8){
   if (document.getElementById("display").innerHTML=="0"){
      document.getElementById("display").innerHTML = document.getElementById("display").innerHTML.replace("0","2");
   }
@@ -143,10 +139,8 @@ if (disp_b.length < 8){
 
 var tecla1 = document.getElementById("1");
 tecla1.addEventListener("click",function(){
-       var disp = document.getElementById("display").innerHTML.replace(/ /g,"");
-       var disp_a = disp.replace("-","");
-       var disp_b = disp_a.replace(".","");
-if (disp_b.length < 8){
+
+if (lpan() < 8){
   if (document.getElementById("display").innerHTML=="0"){
      document.getElementById("display").innerHTML = document.getElementById("display").innerHTML.replace("0","1");
   }
@@ -164,10 +158,8 @@ teclaon.addEventListener("click",function(){
 
 var teclapunto = document.getElementById("punto");
 teclapunto.addEventListener("click",function(){
-  var disp = document.getElementById("display").innerHTML.replace(/ /g,"");
-  var disp_a = disp.replace("-","");
-  var disp_b = disp_a.replace(".","");
-  if (disp_b.length < 8){
+
+  if (lpan() < 8){
     if (document.getElementById("display").innerHTML.indexOf(".")=="-1"){
        document.getElementById("display").innerHTML = document.getElementById("display").innerHTML.concat(".");
     }
@@ -176,10 +168,8 @@ teclapunto.addEventListener("click",function(){
 
 var teclasigno = document.getElementById("sign");
 teclasigno.addEventListener("click",function(){
-  var disp = document.getElementById("display").innerHTML.replace(/ /g,"");
-  var disp_a = disp.replace("-","");
-  var disp_b = disp_a.replace(".","");
-  if (disp_b.length < 8){
+
+  if (lpan() <= 8){
     if(document.getElementById("display").innerHTML!="0"){
       if (document.getElementById("display").innerHTML.indexOf("-")=="-1"){
          document.getElementById("display").innerHTML = "-" + document.getElementById("display").innerHTML;
